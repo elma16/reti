1fdc8dd04b612da4424467f33175d06ad0bc1349
elma16 <ucabem1@ucl.ac.uk>
2022-04-05 18:22:53 +0100

    Stabilize FCE table script output handling

--- FCE-table.sh
+++ FCE-table.sh
@@ -2,32 +2,32 @@
 
 # setup should be .cql -i DATABASE SCRIPTS
 
-cql6/cql -i cql6/sample.pgn cql6/reti/1-4BN.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/2-0Pp.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/2-1KPk.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/3-1Np.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/3-2NN.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/4-1Bp.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/4-2scBB.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/4-3ocBB.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/5-0BvsN.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/6-1-0RP.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/6-2-1RPr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/6-2-2RPPr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/6-2Rr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/6-3RRrr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/7-1RN.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/7-2RB.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/8-1RNr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/8-2RBr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/8-3RAra.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/9-1Qp.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/9-2Qq.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/9-3QPq.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-1Qa.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-2Qr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-3Qaa.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-4Qra.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-5Qrr.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-6Qaaa.cql
-cql6/cql -i cql6/sample.pgn cql6/reti/10-7QAq.cql
+cql6/cql -i $1 -o cql6/output/$1/1-4BN.pgn cql6/reti/1-4BN.cql
+cql6/cql -i $1 -o cql6/output/$1/2-0Pp.pgn cql6/reti/2-0Pp.cql
+cql6/cql -i $1 -o cql6/output/$1/2-1KPk.pgn cql6/reti/2-1KPk.cql
+cql6/cql -i $1 -o cql6/output/$1/3-1Np.pgn cql6/reti/3-1Np.cql
+cql6/cql -i $1 -o cql6/output/$1/3-2NN.pgn cql6/reti/3-2NN.cql
+cql6/cql -i $1 -o cql6/output/$1/4-1Bp.pgn cql6/reti/4-1Bp.cql
+cql6/cql -i $1 -o cql6/output/$1/4-2scBB.pgn cql6/reti/4-2scBB.cql
+cql6/cql -i $1 -o cql6/output/$1/4-3ocBB.pgn cql6/reti/4-3ocBB.cql
+cql6/cql -i $1 -o cql6/output/$1/5-0BvsN.pgn cql6/reti/5-0BvsN.cql
+cql6/cql -i $1 -o cql6/output/$1/6-1-0RP.pgn cql6/reti/6-1-0RP.cql
+cql6/cql -i $1 -o cql6/output/$1/6-2-1RPr.pgn cql6/reti/6-2-1RPr.cql
+cql6/cql -i $1 -o cql6/output/$1/6-2-2RPPr.pgn cql6/reti/6-2-2RPPr.cql
+cql6/cql -i $1 -o cql6/output/$1/6-2Rr.pgn cql6/reti/6-2Rr.cql
+cql6/cql -i $1 -o cql6/output/$1/6-3RRrr.pgn cql6/reti/6-3RRrr.cql
+cql6/cql -i $1 -o cql6/output/$1/7-1RN.pgn cql6/reti/7-1RN.cql
+cql6/cql -i $1 -o cql6/output/$1/7-2RB.pgn cql6/reti/7-2RB.cql
+cql6/cql -i $1 -o cql6/output/$1/8-1RNr.pgn cql6/reti/8-1RNr.cql
+cql6/cql -i $1 -o cql6/output/$1/8-2RBr.pgn cql6/reti/8-2RBr.cql
+cql6/cql -i $1 -o cql6/output/$1/8-3RAra.pgn cql6/reti/8-3RAra.cql
+cql6/cql -i $1 -o cql6/output/$1/9-1Qp.pgn cql6/reti/9-1Qp.cql
+cql6/cql -i $1 -o cql6/output/$1/9-2Qq.pgn cql6/reti/9-2Qq.cql
+cql6/cql -i $1 -o cql6/output/$1/9-3QPq.pgn cql6/reti/9-3QPq.cql
+cql6/cql -i $1 -o cql6/output/$1/10-1Qa.pgn cql6/reti/10-1Qa.cql
+cql6/cql -i $1 -o cql6/output/$1/10-2Qr.pgn cql6/reti/10-2Qr.cql
+cql6/cql -i $1 -o cql6/output/$1/10-3Qaa.pgn cql6/reti/10-3Qaa.cql
+cql6/cql -i $1 -o cql6/output/$1/10-4Qra.pgn cql6/reti/10-4Qra.cql
+cql6/cql -i $1 -o cql6/output/$1/10-5Qrr.pgn cql6/reti/10-5Qrr.cql
+cql6/cql -i $1 -o cql6/output/$1/10-6Qaaa.pgn cql6/reti/10-6Qaaa.cql
+cql6/cql -i $1 -o cql6/output/$1/10-7Aq.pgn cql6/reti/10-7QAq.cql
