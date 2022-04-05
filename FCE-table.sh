2ce84fcb0d11af13fff7c186b3c38e6fc3ffb49e
elma16 <ucabem1@ucl.ac.uk>
2022-04-05 17:40:48 +0100

    Add initial Fundamental Chess Endgames CQL scripts

---
+++ FCE-table.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# setup should be .cql -i DATABASE SCRIPTS
+
+cql6/cql -i cql6/sample.pgn cql6/reti/1-4BN.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/2-0Pp.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/2-1KPk.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/3-1Np.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/3-2NN.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/4-1Bp.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/4-2scBB.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/4-3ocBB.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/5-0BvsN.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/6-1-0RP.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/6-2-1RPr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/6-2-2RPPr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/6-2Rr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/6-3RRrr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/7-1RN.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/7-2RB.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/8-1RNr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/8-2RBr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/8-3RAra.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/9-1Qp.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/9-2Qq.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/9-3QPq.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-1Qa.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-2Qr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-3Qaa.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-4Qra.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-5Qrr.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-6Qaaa.cql
+cql6/cql -i cql6/sample.pgn cql6/reti/10-7QAq.cql
