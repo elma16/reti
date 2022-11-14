92eabea1b11aea48d8f6c776f3f5fd60dab3784b
elma16 <ucabem1@ucl.ac.uk>
2022-11-14 10:10:59 +0000

    Adjust the training module after editor conflict cleanup

--- src/reti/training.py
+++ src/reti/training.py
@@ -2,35 +2,31 @@ import webbrowser
 import chess
 import numpy as np
 
-class EndgameTraining(Training):
-    def __init__(self):
-        self.white = None
-        self.black = None
+class EndgameTraining:
+    def __init__(self, white, black):
+        self.white = white
+        self.black = black
     def random_generate(self):
         '''
         Generate a training game by randomly making a position
         '''
         board = chess.Board()
-        pieces = white + black
+        pieces = self.white + self.black
         pieces = [chess.Piece.from_symbol(x) for x in pieces]
+        print(pieces)
+        isvalid = False
+        while isvalid == False:
+            squares = np.random.choice(64, len(pieces), replace=False)
+            for idx in range(len(pieces)):
+                board.set_piece_at(squares[idx], pieces[idx])
+            if board.is_valid():
+                isvalid = True
+                return board
 
-        # clear board
-        for square in chess.SQUARES:
-            board.remove_piece_at(square)
-        
-        while not board.is_valid() and board.is_empty():
-        squares = np.random.choice([0,63], len(pieces), replace=False)
-
-        for idx in range(len(pieces)):
-            board.set_piece_at(squares[idx], pieces[idx])
-        if board.is_valid():
-            return board
-        else:
-            return None
-            
-        fen = '////// w KQkq - 0 1'
+        # fen of the board
+        fen = board.fen()
+        print(fen)
         webbrowser.open('https://lichess.org/editor/'+fen.replace(' ','_'))
-        pass
     def game_generate(self):
         '''
         Generate a training game by cql
