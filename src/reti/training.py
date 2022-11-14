dee8c190fe85cb3ec7f7b548057be8e7f1ee725e
elma16 <ucabem1@ucl.ac.uk>
2022-11-14 20:27:48 +0000

    Add CQL-backed endgame practice support

--- src/reti/training.py
+++ src/reti/training.py
@@ -2,6 +2,7 @@ import webbrowser
 import chess
 import numpy as np
 import requests
+from reti import utilities
 
 
 class EndgameTraining:
@@ -28,11 +29,28 @@ class EndgameTraining:
                 if r.json()['category'] == self.required_result:
                     isvalid = True
         webbrowser.open('https://lichess.org/editor/'+board.fen().replace(' ','_'))
-    def game_generate(self):
+        
+    def game_generate(self, cqlpath, subset_len):
+        '''
+        Generate a training game by cql
+        '''
+        fen, white, black = utilities.print_relevant_positions(cqlpath)
+        assert(len(fen) > subset_len)
+        for count in range(subset_len):
+            print('White: ', white[count])
+            print('Black: ', black[count])
+            url = 'https://lichess.org/editor/'+fen[count].replace(' ', '_')
+            r = requests.get('http://tablebase.lichess.ovh/standard?fen={}'.format(fen[count]))
+            if r.json()['category'] == self.required_result:
+                webbrowser.open(url)
+
+    def game_generate_alt(self):
         '''
         Generate a training game by cql
         '''
         pass
+        
+
     def logical_generate(self):
         '''
         Generate a training game by randomly making a position from logical rules
