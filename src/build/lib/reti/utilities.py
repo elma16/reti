d1214573780c3a031b496e073e423ae32edc56d0
elma16 <ucabem1@ucl.ac.uk>
2022-11-14 20:27:48 +0000

    adding cql ending practice

--- src/build/lib/reti/utilities.py
+++ src/build/lib/reti/utilities.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 import re
+import subprocess
+import glob
+import requests
+import chess
+import webbrowser
+import os
+import sys
+import chess
+import chess.pgn
 
 def game_length_array(content):
   game_array = re.findall(r'\d{1,3}\.\s',content)
@@ -24,3 +33,84 @@ def load_pgn(path):
     with open(path, 'r', encoding=enc) as file:
         content = file.read()
     return content
+
+def merge_pgns(path):
+    '''
+    Given the directory with all the pgns in it, output a pgn with all the results in one file
+    '''
+    dir = path+'/*.pgn'
+    read_files = glob.glob(dir)
+    with open("0all.pgn", "wb") as outfile:
+        for f in read_files:
+            with open(f, "rb") as infile:
+                outfile.write(infile.read())
+
+def fen2tex(tex_file_name, img_dir):
+    # write tex file
+    with open(tex_file_name, 'w') as f:
+        f.write(r'''\documentclass{article}
+    \usepackage{graphicx}
+    \date{}
+    \title{Elliott's Games!}
+    \begin{document}
+    \maketitle
+    \centering
+    Today's set of puzzles are mostly taken from the 2021 Online London Chess League, and a couple from the 2000 Bundesliga in Germany.
+    Once again, if you get stuck, ask one of the coaches to come and help! Write your solutions \textbf{in notation}. \n''')
+        for img in os.listdir(img_dir):
+            #print full path of image
+            img_path = os.path.join(img_dir, img)
+            f.write(r'\includegraphics[width=6cm, height=6cm]{'+img_path+'}\n')
+        f.write(r'''\end{document}''')
+
+    print('tex file written!')
+
+    # execute it in latex
+    subprocess.call(['pdflatex', 'test.tex'])
+    
+    # open the pdf file
+    if sys.platform == 'darwin':
+        subprocess.call(('open', 'test.pdf'))
+    elif os.name == 'nt':
+        os.startfile('test.pdf')
+    elif os.name == 'posix':
+        subprocess.call(('xdg-open', 'test.pdf'))
+
+def fen2png(fen,img):
+    '''
+    Given a fen string, use the website fen2png to output a png of that fen string.
+    '''
+    fen = fen.replace(' ', '%20')
+    img_url = 'https://fen2png.com/api/?fen={}&raw=true'.format(fen)
+    with open(img, 'wb') as f:
+        f.write(requests.get(img_url).content)
+
+def print_relevant_positions(path):
+    '''
+    Given a path to a pgn file already processed by CQL6, print the relevant positions in the pgn file.
+    '''
+    fen_list = []
+    white_player = []
+    black_player = []
+    pgn = open(path, encoding='utf-8', errors='replace')
+    with open(path, encoding="utf-8",errors='replace') as file:
+        content = file.read()
+    num_games = game_length_array(content)[0]
+    print('number of games:',num_games)
+    for igame in range(num_games):
+        game = chess.pgn.read_game(pgn)
+        board = game.board()
+        for node in game.mainline():
+            move = node.move
+            comment = node.comment 
+            board.push(move)
+            if comment == 'CQL':
+                fen_list.append(board.fen())
+                white_player.append(game.headers['White'])
+                black_player.append(game.headers['Black'])
+                break
+        else:
+            continue
+    return fen_list, white_player, black_player
+
+
