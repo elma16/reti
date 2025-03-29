0cd8baf78aef04e40ea5d82b24f0ec314bcac9b0
elma16 <ucabem1@ucl.ac.uk>
2025-03-29 22:21:09 +0000

    Format training scripts and reti CLI modules

--- src/reti/imageoverlay.py
+++ src/reti/imageoverlay.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python3
-
-'''
+"""
 Overlay all the images representing games from a given pgn.
-'''
+"""
+
 import chess.pgn
 import chess.svg
 import sys
-import os, shutil
+import os
+import shutil
 import cv2
 import cairosvg
 import re
 
-dir = 'svg-output'
-dir_png = 'png-output'
+dir = "svg-output"
+dir_png = "png-output"
 
-for line in [dir,dir_png]:
+for line in [dir, dir_png]:
     shutil.rmtree(line)
     os.mkdir(line)
 
@@ -23,8 +23,9 @@ pgn = open(sys.argv[1])
 with open(sys.argv[1]) as f:
     content = str(f.readlines())
 
-number_of_games = len(re.findall(r'Event ', content))
+number_of_games = len(re.findall(r"Event ", content))
 len_of_subgame = int(sys.argv[2])
+
 
 def make_images():
     for idx in range(number_of_games):
@@ -34,21 +35,22 @@ def make_images():
         for move in range(len_of_subgame):
             board.push(game_list[move])
             svg = chess.svg.board(board)
-        with open(f'{dir}/board-{idx}.svg','w') as fh:
+        with open(f"{dir}/board-{idx}.svg", "w") as fh:
             fh.write(svg)
-            svg_name='{}/board-{}.svg'.format(dir,idx)
-            png_name='{}/board-{}.png'.format(dir_png,idx)
-            cairosvg.svg2png(url=svg_name,write_to=png_name)
+            svg_name = "{}/board-{}.svg".format(dir, idx)
+            png_name = "{}/board-{}.png".format(dir_png, idx)
+            cairosvg.svg2png(url=svg_name, write_to=png_name)
     return 0
+
 
 make_images()
 images = []
 for filename in os.listdir(dir_png):
-    img = cv2.imread(os.path.join(dir_png,filename))
+    img = cv2.imread(os.path.join(dir_png, filename))
     images.append(img)
 
 weighted_images = [x / number_of_games for x in images]
 blended_image = sum(weighted_images)
 
-bi_dir = 'blend.png'
+bi_dir = "blend.png"
 cv2.imwrite(bi_dir, blended_image)
